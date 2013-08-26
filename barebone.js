d7f26ced7f4cac86a7d82d9ef3e3c6b800f630b2
Jason Laster <jlaster@etsy.com>
2013-08-25 21:58:56 -0400

    step 1: clean version

--- barebone.js
+++ barebone.js
@@ -1,66 +1,13 @@
 var _ = require(__dirname + '/vendor/underscore');
-var extend = require(__dirname + '/lib/extend');
 var Barebone = {};
 
-Barebone.Model = function(attributes) {
-  attributes || (attributes = {});
 
+Barebone.Model = function() {
   this.attributes = {};
-  this.events = {};
-  
-  this.set(this.defaults);
-  this.initialize(attributes);
 }
 
 _.extend(Barebone.Model.prototype, {
-  initialize: function(attributes, options) {
-  },
-
-  set: function(attributes) {
-
-    var now = this.attributes;
-
-    for(var attr in attributes) {
-      var value = attributes[attr];
-
-      if (now[attr] != value) {
-        this.attributes[attr] = value;
-        this.trigger('change:'+attr, this, value);
-      }
-
-    }
-    return this;
-  },
-
-  get: function(attribute) {
-    return this.attributes[attribute];
-  }
-
 });
 
-Barebone.Events = {
-  bind: function(event, callback) {
-    this.events[event] || (this.events[event] = [])
-    this.events[event].push(callback);
-  },
 
-  trigger: function(event) {
-    var callbacks = this.events[event];
-    if (!callbacks) {
-      return this;
-    }
-
-    var args = Array.prototype.slice.call(arguments, 1);
-
-    for (i = 0, l = callbacks.length; i < l; i++) {
-      var callback = callbacks[i];
-      callback.apply(this, args);
-    }
-  }
-}
-_.extend(Barebone.Model.prototype, Barebone.Events);
-
-
-
-Barebone.Model.extend = extend;
 module.exports = Barebone;
